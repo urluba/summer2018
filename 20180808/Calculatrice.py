cb7d5faba8c6ce8b09834d44136db93b5d87cae0
TristanGRZ77 <t.bouteville@gmail.com>
2018-08-08 14:56:33 +0200

    Calculatrice sans test envoyé sur un autre fichier

--- 20180808/Calculatrice.py
+++ 20180808/Calculatrice.py
@@ -1,51 +1,46 @@
-#def erreur_nombre("calcul.txt", "erreur.txt"):
-#    fs = open("calcul.txt", "r")
-#    fd = open("erreur.txt", "w")
-#    fd.write(raw_input("La valeur {0} n'est pas un nombre".format(_saisie)))
-#    fs.close()
-#    fd.close()
-
 def saisie(_saisie):
     try:
         int(_saisie)
         return int(_saisie)
+        return True
     except:
-        print("La valeur {0} n'est pas un nombre".format(_saisie))
         return False
 
-def saisie2(_saisie2):
-    if ('+' in s) | ('-' in s) | ('*' in s) | ('/' in s):
-        _saisie2 = True
-    else:
-        _saisie2 = False
-
-def operation(x,y, s):
-    if (saisie(x) != False) & (saisie(y) != False) & (saisie2 != False):
-        if '+' in s:
+def operation(x,y, _saisie2):
+    if (saisie(x) != False) & (saisie(y) != False) & (_saisie2 != False):
+        if '+' in _saisie2:
             print(saisie(x) + saisie(y))
-        if '-' in s:
+        if '-' in _saisie2:
             print(saisie(x) - saisie(y))
-        if '*' in s:
+        if '*' in _saisie2:
             print(saisie(x) * saisie(y))
-        if '/' in s:
+        if '/' in _saisie2:
             print(saisie(x) / saisie(y))
 
-fichier = open("calcul.txt", "w")
-fichier.write(raw_input("Saisir la premiere valeur: "))
-fichier.write("\n")
-fichier.write(raw_input("Saisir la seconde valeur: "))
-fichier.write("\n")
-fichier.write(raw_input("Saisir le signe operatoire (+;-;*;/): "))
-fichier.close()   
+#fichier = open("calcul.txt", "w")
+#fichier.write(raw_input("Saisir la premiere valeur: "))
+#fichier.write("\n")
+#fichier.write(raw_input("Saisir la seconde valeur: "))
+#fichier.write("\n")
+#fichier.write(raw_input("Saisir le signe operatoire (+;-;*;/): "))
+#fichier.close()   
 
 fichier = open("calcul.txt", "r")
 x = fichier.readline()
 y = fichier.readline()
-s = fichier.readline()
+_saisie2 = fichier.readline()
 fichier.close()
 
-i = True
-while i:
-    if ('+' in s) | ('-' in s) | ('*' in s) | ('/' in s):
-        operation(x,y, s)
-        break
+operation(x,y, _saisie2)
+if (saisie(x) != True):
+    fichier2 = open("erreur.txt", "w")
+    fichier2.write("La valeur {0} n'est pas un nombre")
+    fichier2.close()
+if (saisie(y) != True):
+    fichier2 = open("erreur.txt", "a")
+    fichier2.write("La valeur {0} n'est pas un nombre")
+    fichier2.close()
+if (_saisie2 != True):
+    fichier2 = open("erreur.txt", "a")
+    fichier2.write("La valeur {0} n'est pas un signe")
+    fichier2.close()
