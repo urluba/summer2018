676a2fefbf64fd5225f7111b2a8b9cb7d78ede55
TristanGRZ77 <t.bouteville@gmail.com>
2018-08-08 11:34:43 +0200

    Écriture et lecture d'un fichier

--- 20180808/Calculatrice.py
+++ 20180808/Calculatrice.py
@@ -1,3 +1,16 @@
 fichier = open("calcul.txt", "w")
-fichier.write(raw_input("Saisir les deux valeurs et le signe operatoire sous le format 0;0;+ : "))
+fichier.write(raw_input("Saisir la premiere valeur: "))
+fichier.write("\n")
+fichier.write(raw_input("Saisir la seconde valeur: "))
+fichier.write("\n")
+fichier.write(raw_input("Saisir le signe operatoire: "))
+fichier.close()   
+
+fichier = open("calcul.txt", "r")
+x = fichier.readline()
+y = fichier.readline()
+s = fichier.readline()
+print x
+print y
+print s
 fichier.close()
